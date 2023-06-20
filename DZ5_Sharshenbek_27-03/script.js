8c283f4436e74592e9e907eb4be88334926719c1
Manap Sharshenbek <sharshenbek05@gmail.com>
2023-06-20 22:46:36 +0600

    lesson5

---
+++ DZ5_Sharshenbek_27-03/script.js
@@ -0,0 +1,15 @@
+const books = ['Война и мир', 'унесенные призраками', 'Улисс', 'Управление в условиях кризиса. Как выжить и стать сильнее','римрвимроваимров'];
+function findBooks(){
+    const bookWithU = []
+    const bookWithoutU = []
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].toLowerCase().includes('у')) {
+            bookWithU.push(books[i]);
+        } else {
+            bookWithoutU.push(books[i]);
+        }
+    }
+    console.log('Книги с у',bookWithU);
+    console.log('Книги без у',bookWithoutU);
+}
+findBooks(books);
